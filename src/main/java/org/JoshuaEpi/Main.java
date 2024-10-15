a46a34c405e025dbba25b927b599da63a38bbc88
Joshua <joshuaterrones18@icloud.com>
2024-10-14 23:02:55 -0500

    calculadora terminada

--- src/main/java/org/JoshuaEpi/Main.java
+++ src/main/java/org/JoshuaEpi/Main.java
@@ -1,17 +1,54 @@
 package org.JoshuaEpi;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        char operator;
+        double number1, number2, result;
+//creacion de un objeto de clase Scanner
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // preguntar al usuario para ingresar un signo
+        System.out.print("Enter operator: +, -, *, or /");
+        operator = input.next().charAt(0);
+        //ingresar numeros
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+        switch (operator) {
+
+            // performs addition between numbers
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+            // performs subtraction between numbers
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+
+            // performs multiplication between numbers
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+
+            // performs division between numbers
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
         }
+
+        input.close();
     }
 }
